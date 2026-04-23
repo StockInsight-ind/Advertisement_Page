02c6e917730c2ebf4d54fab1520f4c97cfda5a65
Stock Insight <stockinsights214@gmail.com>
2026-04-23 19:45:42 +0530

    Add files via upload

---
+++ Advertisement_Page-main/config.js
@@ -0,0 +1,5 @@
+// config.js
+window.ENV = {
+  SUPABASE_URL: "https://mnbmocinletzjsvmaaev.supabase.co",
+  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uYm1vY2lubGV0empzdm1hYWV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MzYzNzEsImV4cCI6MjA5MTMxMjM3MX0.X_bbX8_ea3x8XkZiYQ7KOqBWBUklhf0-L5GW9Y0R6LA"
+};
